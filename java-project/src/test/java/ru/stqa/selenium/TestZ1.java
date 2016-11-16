d26dfe2a633c0cb665c56941a15fb066e3ca6f56
Anna Falileeva <laf2009@yandex.ru>
2016-11-17 01:16:31 +0300

    Задание 1 - последняя версия

--- java-project/src/test/java/ru/stqa/selenium/TestZ1.java
+++ java-project/src/test/java/ru/stqa/selenium/TestZ1.java
@@ -3,20 +3,15 @@ package ru.stqa.selenium;
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class TestZ1 {
 
     private WebDriver driver;
-    private WebDriverWait wait;
-
 
     @Before
     public void start() {
-
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver,10);
-
     }
 
     @Test
