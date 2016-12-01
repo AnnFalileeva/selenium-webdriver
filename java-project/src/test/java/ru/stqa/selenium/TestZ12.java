3c276fe228ceac7cce1b21bc9fd434f213aeb346
Anna Falileeva <laf2009@yandex.ru>
2016-12-02 00:20:30 +0300

    Задание 12

--- java-project/src/test/java/ru/stqa/selenium/TestZ12.java
+++ java-project/src/test/java/ru/stqa/selenium/TestZ12.java
@@ -2,6 +2,7 @@ package ru.stqa.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class TestZ12 extends TestBase {
@@ -18,9 +19,13 @@ public class TestZ12 extends TestBase {
         // General
         driver.findElement(By.cssSelector("[name=status][value='1']")).click();
         driver.findElement(By.cssSelector("[name*=name]")).sendKeys("Новая уточка");
-        driver.findElement(By.cssSelector("[name*=code]")).sendKeys("123");
-
+        driver.findElement(By.cssSelector("[name=code]")).sendKeys("123");
         driver.findElement(By.cssSelector("[name*=categories][value='1']")).click();
+        driver.findElement(By.cssSelector("div.input-wrapper:last-child")).click();
+        driver.findElement(By.cssSelector("[name=quantity]")).sendKeys("1.50");
+
+        Select soldOut = new Select(driver.findElement(By.cssSelector("[name=sold_out_status_id]")));
+        soldOut.selectByValue("2");
 
         // Information
 
