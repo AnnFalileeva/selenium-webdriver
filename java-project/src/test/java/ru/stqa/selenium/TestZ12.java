03b358f8abf0d09331a13e9767b512723eacf23c
Anna Falileeva <laf2009@yandex.ru>
2016-12-02 19:39:13 +0300

    Задание 12

--- java-project/src/test/java/ru/stqa/selenium/TestZ12.java
+++ java-project/src/test/java/ru/stqa/selenium/TestZ12.java
@@ -2,7 +2,13 @@ package ru.stqa.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 
 public class TestZ12 extends TestBase {
@@ -21,17 +27,32 @@ public class TestZ12 extends TestBase {
         driver.findElement(By.cssSelector("[name*=name]")).sendKeys("Новая уточка");
         driver.findElement(By.cssSelector("[name=code]")).sendKeys("123");
         driver.findElement(By.cssSelector("[name*=categories][value='1']")).click();
-        driver.findElement(By.cssSelector("div.input-wrapper:last-child")).click();
+        driver.findElement(By.cssSelector("[value='1-2']")).click();
+
+        driver.findElement(By.cssSelector("[name=quantity]")).clear();
         driver.findElement(By.cssSelector("[name=quantity]")).sendKeys("1.50");
 
         Select soldOut = new Select(driver.findElement(By.cssSelector("[name=sold_out_status_id]")));
         soldOut.selectByValue("2");
+
+        driver.findElement(By.cssSelector("[type=file]")).sendKeys((new File("/1.jpg").getAbsolutePath()));
+
+        driver.findElement(By.cssSelector("[name=date_valid_from]")).sendKeys("12-14-2016");
+        driver.findElement(By.cssSelector("[name=date_valid_to]")).sendKeys("12-27-2016");
+       
+        driver.findElement(By.cssSelector("[name=save]")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector(".dataTable")));
 
         // Information
 
         // Prices
 
     }
+
+    //public void setDatepicker(String cssSelector, String date) {
+
+    //    JavascriptExecutor.class.cast(driver).executeScript(String.format("$('%s').datepicker('setDate', '%s')", cssSelector, date));
+    //}
 
 
 
