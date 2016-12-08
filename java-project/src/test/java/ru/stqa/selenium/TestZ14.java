50082ba73965ca051ee4e866b2b518d86e7c729d
Anna Falileeva <laf2009@yandex.ru>
2016-12-08 22:12:01 +0300

    Задание 14

--- java-project/src/test/java/ru/stqa/selenium/TestZ14.java
+++ java-project/src/test/java/ru/stqa/selenium/TestZ14.java
@@ -2,6 +2,13 @@ package ru.stqa.selenium;
 
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import java.util.List;
+import java.util.Set;
 
 public class TestZ14 extends TestBase {
 
@@ -11,6 +18,31 @@ public class TestZ14 extends TestBase {
         loginToLitecartAdmin();
         driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
 
+        driver.findElement(By.cssSelector("#content .button")).click();
+        List<WebElement> refs = driver.findElements(By.cssSelector("#content table:not(#table-zones) a:not(#address-format-hint)"));
+
+        for (WebElement ref:refs){
+            String orWindow = driver.getWindowHandle();
+            Set<String> exWindows = driver.getWindowHandles();
+            ref.click();
+            String newWindow = wait.until(anyOtherWindow(exWindows));
+            driver.switchTo().window(newWindow);
+            driver.close();
+            driver.switchTo().window(orWindow);
+        }
+
+
+    }
+
+    public ExpectedCondition<String> anyOtherWindow(Set<String> oldWindows){
+        return new ExpectedCondition<String>() {
+            //@Override
+            public String apply(WebDriver driver) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(oldWindows);
+                return handles.size()>0?handles.iterator().next():null;
+            }
+        };
 
     }
 }
